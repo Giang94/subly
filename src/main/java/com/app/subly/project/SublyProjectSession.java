95e8234c74f3f55164c1730d49df0979d818a94b
giangnguyen-icario <15278909+Giang94@users.noreply.github.com>
2025-09-23 13:23:55 +0700

    Implement save/load functions

---
+++ src/main/java/com/app/subly/project/SublyProjectSession.java
@@ -0,0 +1,70 @@
+package com.app.subly.project;
+
+import com.app.subly.model.SublySettings;
+import com.app.subly.persistence.AppSettingsManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+public class SublyProjectSession {
+
+    private SublySettings settings;
+    private File projectFile;
+    private boolean dirty;
+
+    public SublyProjectSession(SublySettings initial) {
+        this.settings = initial;
+        this.dirty = false;
+    }
+
+    public static SublyProjectSession newSessionWithDefaults() throws IOException {
+        // Start from app defaults/resources
+        return new SublyProjectSession(AppSettingsManager.load());
+    }
+
+    public static SublyProjectSession loadFrom(File file) throws IOException {
+        SublyProjectSession s = new SublyProjectSession(AppSettingsManager.loadFromFile(file));
+        s.projectFile = file;
+        s.dirty = false;
+        return s;
+    }
+
+    public SublySettings getSettings() {
+        return settings;
+    }
+
+    public File getProjectFile() {
+        return projectFile;
+    }
+
+    public void setProjectFile(File file) {
+        this.projectFile = file;
+    }
+
+    public boolean hasCurrentFile() {
+        return this.projectFile != null;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    // Apply any UI change to the in-memory settings
+    public void update(Consumer<SublySettings> mutator) {
+        mutator.accept(settings);
+        dirty = true;
+    }
+
+//    public void save(TableView<Subtitle> subtitleTableView) throws IOException {
+//        SublyProjectFileManager fileManager = new SublyProjectFileManager();
+//        fileManager.save(subtitleTableView, this);
+//        dirty = false;
+//    }
+
+//    public void saveAs(File file) throws IOException {
+//        SublyProjectFileManager.saveProject(settings, projectFile);
+//        this.projectFile = file;
+//        dirty = false;
+//    }
+}
